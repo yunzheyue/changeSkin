a80c521a6bdf161462356a1b7561a7452c607766
yunzheyue123 <1301002716@qq.com>
2018-09-12 15:01:36 +0800

    sss

--- src/main/java/com/example/app4/MainActivity.java
+++ src/main/java/com/example/app4/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.app4;
 
+import android.os.Environment;
 import android.preference.Preference;
 import android.preference.SwitchPreference;
 import android.support.v7.app.AppCompatActivity;
@@ -7,10 +8,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.app4.loader.CustomSDCardLoader;
 
+import java.io.File;
+
 import skin.support.SkinCompatManager;
+import skin.support.content.res.ColorState;
+import skin.support.content.res.SkinCompatUserThemeManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (!boolValue) {
                     boolValue = true;
-                    SkinCompatManager.getInstance().loadSkin("night.skin",  new SkinCompatManager.SkinLoaderListener() {
+                    SkinCompatManager.getInstance().loadSkin("night.skin", new SkinCompatManager.SkinLoaderListener() {
                         @Override
                         public void onStart() {
                             Log.e("TAG", "----start");
@@ -54,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
                         @Override
                         public void onFailed(String errMsg) {
-                            Log.e("TAG", "----onFailed---"+errMsg);
+                            Log.e("TAG", "----onFailed---" + errMsg);
                         }
                     }, SkinCompatManager.SKIN_LOADER_STRATEGY_ASSETS);
                 } else {
@@ -84,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
 
                         @Override
                         public void onFailed(String errMsg) {
-                            Log.e("TAG", "----onFailed---"+errMsg);
+                            Log.e("TAG", "----onFailed---" + errMsg);
                         }
                     }, CustomSDCardLoader.SKIN_LOADER_STRATEGY_SDCARD);
                 } else {
@@ -94,6 +100,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        TextView tv_love = findViewById(R.id.tv_love);
+        tv_love.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!boolValue) {
+                    boolValue = true;
+                    SkinCompatUserThemeManager.get().addColorState(R.color.colorPrimary, "#ffffff");
+                    //SkinCompatUserThemeManager.get().addColorState(R.color.colorPrimary, new ColorState.ColorBuilder().a().build());
+                } else {
+                    boolValue = false;
+//                     清除所有已有颜色值。
+//                    SkinCompatUserThemeManager.get().clearColors();
+                }
+            }
+        });
+        TextView tv_like = findViewById(R.id.tv_like);
+        tv_like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!boolValue) {
+                    boolValue = true;
+                    SkinCompatUserThemeManager.get().addDrawablePath(R.mipmap.tab_home_select, Environment.getExternalStorageDirectory() + File.separator + "Pictures/JPEG_20180628_162533.jpg");
+//                     要换肤的资源id，图片路径，图片旋转角度(默认为0)
+//                    SkinCompatUserThemeManager.get().addDrawablePath(R.drawable.windowBackground, "／sdcard/DCIM/Camera/xxx.jpg", 90);
+                } else {
+                    boolValue = false;
+                    // 清除所有已有图片路径。
+//                    SkinCompatUserThemeManager.get().clearDrawables();
+                }
+
+            }
+        });
 
 
     }
